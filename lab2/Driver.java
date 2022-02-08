554e8f9c82f09db2ad8d778052d1fd8747b52e96
Ville <Ville@MSI>
2022-02-08 11:17:15 +0100

    test

--- lab2/Driver.java
+++ lab2/Driver.java
@@ -1,5 +1,7 @@
 package lab2;
 
+import java.awt.Color;
+
 import lab2.level.Level;
 import lab2.level.LevelGUI;
 import lab2.level.Room;
@@ -7,7 +9,11 @@ import lab2.level.Room;
 public class Driver {
 
 	public void run() {
-		System.out.print("This is a print-out from the driver");
+		System.out.println("This is a print-out from the driver");
+		
+		Room first = new Room(5,5,Color.black);
+		Room second = new Room(8,3,Color.red);
+		Room third = new Room(10,10,Color.blue);
 	}
 
 }
--- lab2/Main.java
+++ lab2/Main.java
@@ -9,9 +9,8 @@ import lab2.level.Room;
 public class Main {
 	public static void main(String[] args) {
 		Driver a = new Driver();
-		//a.run();
-		Room first = new Room(5,5,Color.black);
-		//first.connectEastTo(first);
+		a.run();
+		
 		
 	}
 }
