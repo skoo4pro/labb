b41e70948c2fe70ef6a0257efb213f8b616448dc
User <User@130.240.152.209>
2022-02-08 11:17:56 +0100

    tfg

--- lab2/Main.java
+++ lab2/Main.java
@@ -10,8 +10,7 @@ public class Main {
 	public static void main(String[] args) {
 		Driver a = new Driver();
 		a.run();
-		Room first = new Room(5,5,Color.black);
-		first.connectEastTo(first);
+		
 		
 	}
 }
